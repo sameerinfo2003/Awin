998553b8b67c754222080550617d69413f0c7bd2
sameer.srivastava <sameer.srivastava@digital.homeoffice.gov.uk>
2023-05-04 05:25:37 +0100

    Adding bit of logging output configurator code structure

---
+++ src/main/java/org/awin/LoggingOutputConfigurator.java
@@ -0,0 +1,10 @@
+package org.awin;
+
+public class LoggingOutputConfigurator {
+
+    private static boolean isConsole = true;
+    private static boolean isFile = false;
+    private static boolean isEmail = false;
+    private static boolean isApi = false;
+
+}
